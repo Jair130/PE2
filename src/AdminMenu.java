a6916386c47928c8c471f05ded05cded987870e3
Jair130 <100359718+Jair130@users.noreply.github.com>
2022-11-22 19:07:35 +0000

    First changes

---
+++ src/AdminMenu.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jairp
+ */
+
+import java.util.Scanner;
+public class AdminMenu {
+     
+    public static void main (String [] args)
+    static  Scanner = new Scanner (System.in);
+        
+    System.out.println("Welcome Admin Choose an option from the menu");
+    
+    
+
+    }
+
+
